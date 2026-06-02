7114097603b44b53cb0230e8d0898b2987f4875f
Andrew14300f <andrew1430000f@gmail.com>
2026-06-02 02:00:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,190 @@
+// dados.js
+const setores = [
+    {
+        nome: "ZTE H3601P",
+        versao: "V9.0.0P10",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/1GsseYrxyutn3fCrvwFP9m0IU3tFjaubR",
+        notas: [
+            "multipro",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Protocolo ZTE - Modelo Roteador: ZTE H3601P\nFirmware:V9.0.0P10\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do ZTE H3601P., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Protocolo ZTE - ZTE H3601P\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "Protocolo ZTE - ROTEADOR/ ONT: ZTE H3601P\nONU: DATACOM DM986-100\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada "
+        }
+    },
+    {
+        nome: "DM986-416",
+        versao: "V1.10.0",
+        linkVersao: "https://docs.clickip.com.br/devices?search=416",
+        notas: [
+            "user",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: DM986-416\nFirmware:V1.10.0\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:DM986-416\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: DM986-416\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligado"
+        }
+    },
+    {
+        nome: "DM986-414",
+        versao: "V8.6.0",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/1mDZUjvrZ0SFVshyXpOPAKpTDn1sK8xL3",
+        notas: [
+            "user",
+            "onuclickip",
+            "user",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: DM986-414\nFirmware:V1.10.0\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização. ",
+            trocaSenha: "Roteador:DM986-414\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: DM986-414\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada "
+        }
+    },
+    {
+        nome: "DM956 5GT-1800",
+        versao: "V3.0.0",
+        linkVersao: "https://drive.google.com/drive/folders/15bTB9yRDvPdVZX6HdWQU_txFh8acsXrf?usp=drive_link",
+        notas: [
+            "user",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: DM956 5GT-1800 \nONU: DATACOM DM986-100\nFirmware:V3.0.0\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:DM956 5GT-1800\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: DM956 5GT-1800 \nONU: DATACOM DM986-100\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "DM985-424 HW2",
+        versao: "3.2.3",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/1lz1PKpemaRPF5MunlstOkB3-axwckukd",
+        notas: [
+            "support",
+            "onuclickip",
+            "support",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: DM985-424 HW2\nFirmware:3.2.3\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:DM985-424 HW2\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: DM985-424 HW2\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },{
+        nome: "HUAWEI WiFi AX2",
+        versao: "V2.0.0.115",
+        linkVersao: "https://drive.google.com/drive/folders/1NdcUwBz3m4qMYqqGmUwf_AWwLfRaYZHy",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: HUAWEI WiFi AX2\nFirmware:V2.0.0.115\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:HUAWEI WiFi AX2\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: HUAWEI WiFi AX2\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "HUAWEI WiFi AX3",
+        versao: "V2.00.318",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/1qQa5pZJxiHzuEqg7qCgfBrN034w4lcO8",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: HUAWEI WiFi AX3\nFirmware:V2.00.318\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:HUAWEI WiFi AX3\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: HUAWEI WiFi AX3\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "ZTE H198A",
+        versao: "V3.0.0.6",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/1-anZ8bzNX5vWkhRNZzpQ4XRWNzkzq9vC",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: ZTE H198A\nFirmware:V3.0.0.6\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:ZTE H198A\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: ZTE H198A\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "DM955 5GT 1200",
+        versao: "V2.4.0",
+        linkVersao: "https://drive.google.com/drive/folders/1bD2d1LuN5ByoygHZSRciFSwi_Gxf9Y7z",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: DM955 5GT 1200\nFirmware:V2.4.0\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:DM955 5GT 1200\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: DM955 5GT 1200\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "Action RF 1200",
+        versao: "V1.2.3",
+        linkVersao: "https://drive.google.com/drive/folders/1yjUfsHqzjaxVQkxX43qRPpiUxxXXtTRH",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: Action RF 1200\nFirmware:V1.2.3\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:Action RF 1200\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: Action RF 1200\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "Action RG 1200",
+        versao: "V2.1.8",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/17mkQl869wvpp7M8NmLJfbVFIkrMbzlJW",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: Action RG 1200\nFirmware:V2.1.8\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:Action RG 1200\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: Action RG 1200\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+    {
+        nome: "ZTE H199A",
+        versao: "V9.1.0P5_MUL",
+        linkVersao: "https://drive.google.com/drive/u/0/folders/113_knFPtTamTECLhaVUFbbf4fMQLtwqW",
+        notas: [
+            "multipro.",
+            "Onucl1ck1p",
+            "multipro",
+            "0nucl!ck!P"
+        ],
+        scripts: {
+            lentidao: "Modelo Roteador: ZTE H199A\nFirmware:V9.1.0P5_MUL\nAparelhos conectados:5 \nSinal de fibra: \nConsta histórico de desconexões: Não \nVerificações realizadas. Feito otimização de canal na frequência 2.4Ghz/5Ghz do DM986-416., aplicado reboot no roteador cliente confirmou a normalização.",
+            trocaSenha: "Roteador:ZTE H199A\nNova Senha: \nTroca de senha realizada conforme solicitação.\nCliente ciente das informações, também confirma acesso à rede com a nova senha.\nProtocolo de atendimento finalizado.",
+            offline: "ROTEADOR/ ONT: ZTE H199A\nConfigurado: Sim \nStatus de leds: PON: Apagada; LOS: Ligada"
+        }
+    },
+];
